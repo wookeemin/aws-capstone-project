ea6051db9d154f1d9f7291cda51d1796d953d306
wookee min <wookeemin@wookees-Air.fritz.box>
2022-07-19 23:51:27 +0200

    Update Capstone

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'databse1', # database name in RDS is written here
+        'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': config('PASSWORD'),
         'HOST': 'aws-capstone-rds.cqersvev53vc.us-east-1.rds.amazonaws.com',  # database endpoint is written here
